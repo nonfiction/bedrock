1b6f2043e02d35f4c5dfa75cc88ade9776fdbc76
Jon Suderman <jon@suderman.net>
2020-05-22 21:00:13 +0000

    execute run.sh if exists

--- run.sh
+++ run.sh
@@ -4,5 +4,11 @@
 curl https://api.wordpress.org/secret-key/1.1/salt/ | tr -d "[:blank:]" > /tmp/salts
 sed -e "s/define('//g" -e "s/','/='/g" -e "s/);//g" /tmp/salts > /srv/salts.env
 
+# Execute run.sh if it exists
+if [ -e /srv/web/app/run.sh ]; then
+  chmod +x /srv/web/app/run.sh
+  /srv/web/app/run.sh
+fi
+
 # Run Apache in foregrond
 apachectl -D FOREGROUND
