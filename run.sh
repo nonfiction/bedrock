5f90aaf633f137a6a07d6efdcaa8dc9f4690f757
Jon Suderman <jon@suderman.net>
2020-07-08 18:31:02 +0000

    fix cron again

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@
 curl https://api.wordpress.org/secret-key/1.1/salt/ | tr -d "[:blank:]" > /tmp/salts
 sed -e "s/define('//g" -e "s/','/='/g" -e "s/);//g" /tmp/salts > /srv/salts.env
 
+# Run wp-cron every 5 minutes
+echo "*/5 * * * * root curl https://$APP_NAME.$APP_HOST/wp-cron.php?doing_wp_cron > /dev/null 2>&1" >> /etc/crontab
+
 # Execute run.sh if it exists
 if [ -e /srv/web/app/run.sh ]; then
   chmod +x /srv/web/app/run.sh
