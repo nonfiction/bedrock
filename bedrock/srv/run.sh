51637fa54567ef65baa7cd224bc234a33aeb19d3
Jon Suderman <jon@suderman.net>
2020-07-19 20:44:25 +0000

    unzip any archives in plugins/themes

--- bedrock/srv/run.sh
+++ bedrock/srv/run.sh
@@ -5,6 +5,11 @@ for f in /srv/web/app/site/mu-plugins/*; do [ -e $f ] && ln -sf $f /srv/web/app/
 for f in /srv/web/app/site/plugins/*;    do [ -e $f ] && ln -sf $f /srv/web/app/plugins/;    done
 for f in /srv/web/app/site/themes/*;     do [ -e $f ] && ln -sf $f /srv/web/app/themes/;     done
 
+# Extract any zip archives
+for f in /srv/web/app/mu-plugins/*.zip; do [ -e $f ] && unzip $f; done
+for f in /srv/web/app/plugins/*.zip;    do [ -e $f ] && unzip $f; done
+for f in /srv/web/app/themes/*.zip;     do [ -e $f ] && unzip $f; done
+
 # Run wp-cron every 5 minutes
 echo "*/5 * * * * root curl https://$APP_NAME.$APP_HOST/wp/wp-cron.php?doing_wp_cron > /dev/null 2>&1" >> /etc/crontab
 service cron start
