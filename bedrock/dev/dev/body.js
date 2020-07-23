a7c711975e9c235c42252aa026c3f18f74ae3617
Jon Suderman <jon@suderman.net>
2020-07-23 03:55:22 +0000

    post assets in head instead of body

--- bedrock/dev/dev/body.js
+++ bedrock/dev/dev/body.js
@@ -1,9 +1,7 @@
 // This is the <body> entry file for webpack--do not edit!
 
 import '/srv/web/app/site/src/assets/body.js';
-import '/srv/dev/posts-bundle.js';
 
 if (module.hot) {
   module.hot.accept( '/srv/web/app/site/src/assets/body.js' );
-  module.hot.accept( '/srv/dev/posts-bundle.js' );
 }
--- bedrock/dev/dev/head.js
+++ bedrock/dev/dev/head.js
@@ -1,7 +1,9 @@
 // This is the <head> entry file for webpack--do not edit!
 
 import '/srv/web/app/site/src/assets/head.js';
+import '/srv/dev/posts-bundle.js';
 
 if (module.hot) {
   module.hot.accept( '/srv/web/app/site/src/assets/head.js' );
+  module.hot.accept( '/srv/dev/posts-bundle.js' );
 }
